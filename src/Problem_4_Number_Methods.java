f4022e904abc3152631adc95129b9b31f55df0e7
nikohightower@gmail.com <hWUBB-H9ux4aPmK>
2023-06-11 09:26:05 +0300

    Different implementations of createArray method

--- src/Problem_4_Number_Methods.java
+++ src/Problem_4_Number_Methods.java
@@ -5,9 +5,17 @@ public class Problem_4_Number_Methods {
         System.out.println(countDigits(123456789));
         System.out.println(getParity(5));
 
-        int[] array = createArray(12345678);
+        int[] array = createArray(-12345678);
         printArray(array);
+        System.out.println();
 
+        int[] arrayStream = createArrayWithStream(-23456789);
+        printArray(arrayStream);
+        System.out.println();
+
+        int[] arraylog = logCreateArray(23456789);
+        printArray(arraylog);
+        System.out.println();
 
     }
 
@@ -28,27 +36,32 @@ public class Problem_4_Number_Methods {
 
     public static int[] createArray(int a) {
         int[] array = new int[countDigits(a)];
-        int i = 0;
+        int i = array.length - 1;
 
         while (a != 0) {
             int b = a % 10;
             array[i] = b;
             a = a / 10;
-            i++;
+            i--;
         }
 
-        return reverseArray(array);
+        return array;
     }
 
-    public static int[] reverseArray(int[] array) {
-        int[] newArray = new int[array.length];
-        int j = array.length - 1;
-
-        for(int i = 0; i < array.length; i++) {
-            newArray[i] = array[j];
-            j--;
+    public static int[] logCreateArray(int a) {
+        int len = 1 + (int) (Math.log(a) / Math.log(10));
+        int[] array = new int[len];
+        for (int i = 0; i < len; i++) {
+            int pow10 = (int) Math.pow(10, len - i - 1);
+            int digit = a / pow10;
+            array[i] = digit;
+            a -= digit * pow10;
         }
-        return newArray;
+        return array;
+    }
+
+    public static int[] createArrayWithStream(int a) {
+        return String.valueOf(a).chars().map(Character::getNumericValue).toArray();
     }
 
     public static void printArray(int[] array) {
@@ -65,5 +78,17 @@ public class Problem_4_Number_Methods {
         return true;
     }
 
+/*
+public static int[] reverseArray(int[] array) {
+        int[] newArray = new int[array.length];
+        int j = array.length - 1;
+
+        for(int i = 0; i < array.length; i++) {
+            newArray[i] = array[j];
+            j--;
+        }
+        return newArray;
+    }
+ */
 
 }
